8fc87384e70426ae9dd647c25767f0d95eeaf32b
Gregg Carey <gregg.carey@outlook.com>
2025-02-06 16:42:51 -0500

    added copy to clipboard link

---
+++ BlazorSurvey/BlazorSurvey.Client/Pages/Home.razor.js
@@ -0,0 +1,20 @@
+﻿export function CopyToClipboard(url) {
+    window.navigator.clipboard.writeText(url).then(function () {
+        alert("Copied to Clipboard");
+    })
+        .catch(function (error) {
+            alert(error);
+        });
+}
+
+/*
+window.clipboardCopy = {
+    copyText: function (text) {
+        navigator.clipboard.writeText(text).then(function () {
+            alert("Copied to clipboard!");
+        })
+            .catch(function (error) {
+                alert(error);
+            });
+    }
+};*/
